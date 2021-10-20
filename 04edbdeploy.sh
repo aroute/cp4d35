4cc59c4aa952ed3912c89ad37125b21d4c525ebf
Arif <aroute@users.noreply.github.com>
2021-10-20 15:21:39 -0500

    Update and rename 18edbprepare.sh to 04edbdeploy.sh

---
+++ 04edbdeploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd cpd-cli-workspace/
+rm -rf *
+cd ..
+./cpd-cli adm --repo ./repo.yaml --assembly edb-operator --arch x86_64 --accept-all-licenses --namespace zen --verbose
+./cpd-cli adm --repo ./repo.yaml --assembly edb-operator --arch x86_64 --accept-all-licenses --namespace zen --verbose --apply
+
+export REG=$(oc get route image-registry -n openshift-image-registry -o jsonpath='{.spec.host}')
+
+./cpd-cli install \
+--repo repo.yaml \
+--assembly edb-operator \
+--optional-modules edb-pg-base:x86_64 \
+--arch x86_64 \
+--storageclass ${storageclass} \
+--accept-all-licenses \
+--transfer-image-to ${REG}/zen \
+--cluster-pull-prefix image-registry.openshift-image-registry.svc:5000/zen \
+--insecure-skip-tls-verify \
+--target-registry-username $(oc whoami) \
+--target-registry-password $(oc whoami -t) \
+--latest-dependency \
+--namespace zen \
+--verbose
+
+./cpd-cli status --assembly edb-operator --namespace zen
