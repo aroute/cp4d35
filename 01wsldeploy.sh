34605ff5d89bb207a377f5d3ad68922608beff3b
Arif <aroute@users.noreply.github.com>
2021-10-20 13:05:59 -0500

    Update and rename 10wslprepare.sh to 01wsldeploy.sh

---
+++ 01wsldeploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+./cpd-cli adm --repo ./repo.yaml --assembly wsl --arch x86_64 --accept-all-licenses --namespace zen --verbose
+./cpd-cli adm --repo ./repo.yaml --assembly wsl --arch x86_64 --accept-all-licenses --namespace zen --verbose --apply
+
+export REG=$(oc get route image-registry -n openshift-image-registry -o jsonpath='{.spec.host}')
+
+./cpd-cli install \
+--repo ./repo.yaml \
+--assembly wsl \
+--arch x86_64 \
+--namespace zen \
+--storageclass ${storageclass} \
+--accept-all-licenses \
+--transfer-image-to ${REG}/zen \
+--cluster-pull-prefix image-registry.openshift-image-registry.svc:5000/zen \
+--insecure-skip-tls-verify \
+--target-registry-username $(oc whoami) \
+--target-registry-password $(oc whoami -t) \
+--latest-dependency \
+--verbose
+
+./cpd-cli status --assembly wsl --namespace zen
