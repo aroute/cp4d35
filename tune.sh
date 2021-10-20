6f51262246477cd493a43c6b461e22ca9fb437f9
Arif <aroute@users.noreply.github.com>
2021-10-20 15:59:34 -0500

    Update tune.sh

--- tune.sh
+++ tune.sh
@@ -3,5 +3,5 @@
 ENTITLEMENT_KEY=enter_api_key
 oc project kube-system
 oc create secret docker-registry cpregistrysecret --docker-server cp.icr.io/cp/cpd --docker-username cp --docker-password $ENTITLEMENT_KEY --docker-email a@b.c
-oc apply -f setkernelparams.yaml -n kube-system
-oc apply -f norootsquash.yaml -n kube-system
+oc create -f setkernelparams.yaml -n kube-system
+oc create -f norootsquash.yaml -n kube-system
