0a642a4aa5bafa6d979104d8b52a1ce25cf7eaf7
Arif <arifroute@outlook.com>
2021-11-04 18:15:52 +0000

    removed cert

--- 00lite.sh
+++ 00lite.sh
@@ -24,12 +24,12 @@ export REG=$(oc get route image-registry -n openshift-image-registry -o jsonpath
 
 ./cpd-cli status --assembly lite --namespace zen
 
-oc project zen
-nginxpod=$(oc get pod -n zen -l component=ibm-nginx -o jsonpath='{.items[0].metadata.name}')
-oc cp $nginxpod:/etc/nginx/config/ssl/cert.crt cert.crt
-routepolicy=`oc get route -n zen | awk '$1 == "zen-cpd" {print $5}'`
-if [[ $routepolicy != "reencrypt" ]]; then
-oc delete route zen-cpd
-oc create route reencrypt zen-cpd --service=ibm-nginx-svc --port=ibm-nginx-https-port --dest-ca-cert=cert.crt
-oc annotate route zen-cpd --overwrite haproxy.router.openshift.io/timeout=3600s
-fi
+# oc project zen
+# nginxpod=$(oc get pod -n zen -l component=ibm-nginx -o jsonpath='{.items[0].metadata.name}')
+# oc cp $nginxpod:/etc/nginx/config/ssl/cert.crt cert.crt
+# routepolicy=`oc get route -n zen | awk '$1 == "zen-cpd" {print $5}'`
+# if [[ $routepolicy != "reencrypt" ]]; then
+# oc delete route zen-cpd
+# oc create route reencrypt zen-cpd --service=ibm-nginx-svc --port=ibm-nginx-https-port --dest-ca-cert=cert.crt
+# oc annotate route zen-cpd --overwrite haproxy.router.openshift.io/timeout=3600s
+# fi
--- cli.sh
+++ cli.sh
@@ -5,6 +5,7 @@ set -e
 ## This pulls the CLI for Linux. If you are on Mac, replace the URL with Mac specific binary file.
 
 # curl -sLo /tmp/cpd-cli.tar.gz https://github.com/IBM/cpd-cli/releases/download/v3.5.4/cpd-cli-linux-EE-3.5.4.tgz
-curl -sLo /tmp/cpd-cli.tar.gz https://github.com/IBM/cpd-cli/releases/download/v3.5.6/cpd-cli-linux-EE-3.5.6.tgz
+# curl -sLo /tmp/cpd-cli.tar.gz https://github.com/IBM/cpd-cli/releases/download/v3.5.6/cpd-cli-linux-EE-3.5.6.tgz
+curl -sLo /tmp/cpd-cli.tar.gz https://github.com/IBM/cpd-cli/releases/download/v3.5.7/cpd-cli-linux-EE-3.5.7.tgz
 tar xzvf /tmp/cpd-cli.tar.gz -C . 
 rm -rf /tmp/cpd-cli.tar.gz
